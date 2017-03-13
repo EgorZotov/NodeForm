43674d037296d1b8da03e0f559fbf46dd0d69b49
Егор Зотов < >
2017-03-14 00:53:42 +0300

    Добавлен рендеринг страницы при нажатии кнопки и исправлена повторная передача данных

--- routes/result.js
+++ routes/result.js
@@ -7,16 +7,7 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 router.post("/", urlencodedParser, function (request, response) {
     if(!request.body) return response.sendStatus(400);
     console.log(request.body);
-    render(request.body);
-   // console.log(request.body);
+    response.render('result',{data:request.body});
 });
-
-/* GET users listing. */
-function render(data){
-    router.get('/', function(req, res, next) {
-        res.render('result',{data:data});
-    });
-}
-
 
 module.exports = router;
